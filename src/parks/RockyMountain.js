f0192e0bb8fa1ca49350daaa36c0fe4cfe14fa4e
Fella Kettani <fellakettani@gmail.com>
2023-09-11 16:10:30 +0100

    finished assignements

--- src/parks/RockyMountain.js
+++ src/parks/RockyMountain.js
@@ -1,11 +1,12 @@
-const trees = "Aspen and Pine";
+export const trees = "Aspen and Pine";
 
-function wildlife() {
+export default function Wildlife() {
   console.log("Elk, Bighorn Sheep, Moose");
+  return (<h1>Elk, Bighorn Sheep, Moose"</h1>)
 }
 
 function elevation() {
   console.log("9583 ft");
 }
 
-export { trees, wildlife };
+// export { trees, wildlife };
